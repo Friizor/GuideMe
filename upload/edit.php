a128adc7a3d0b14325648bf58900d5887f519112
friizor <mohamedzaidi768@gmail.com>
2024-06-02 18:51:33 +0100

    adding dataController system

--- upload/edit.php
+++ upload/edit.php
@@ -1,3 +1,22 @@
 <?php
-echo 'test';
-?>
+require_once("../system/dataControlle.php");
+$email = $_SESSION['email'];
+$password = $_SESSION['password'];
+if ($email != false && $password != false) {
+    $sql = "SELECT * FROM usertable WHERE email = '$email'";
+    $run_Sql = mysqli_query($con, $sql);
+    if ($run_Sql) {
+        $fetch_info = mysqli_fetch_assoc($run_Sql);
+        $status = $fetch_info['status'];
+        $code = $fetch_info['code'];
+        if ($status == "verified") {
+            if ($code != 0) {
+                header('Location: ../system/reset-code.php');
+            }
+        } else {
+            header('Location: ../system/user-otp.php');
+        }
+    }
+} else {
+    header('Location: ../system/login-user.php');
+}
