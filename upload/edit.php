abdbdac7a40cc569916552f91d1357908514e883
friizor <mohamedzaidi768@gmail.com>
2024-06-02 18:54:26 +0100

    calling pictures files and upload them

--- upload/edit.php
+++ upload/edit.php
@@ -30,3 +30,34 @@ $phoneNumber = $_POST['phone'];
 $emailAddress = $_POST['email'];
 $address = $_POST['address'];
 
+
+//mainImg
+$file_name = $_FILES['mainImg']['name'];
+$tempname = $_FILES['mainImg']['tmp_name'];
+
+//sideImg1
+
+$file_name1 = $_FILES['sidebar']['name'];
+$tempname1 = $_FILES['sidebar']['tmp_name'];
+
+//sideImg2
+
+$file_name2 = $_FILES['sidebar2']['name'];
+$tempname2 = $_FILES['sidebar2']['tmp_name'];
+
+//sideImg3
+
+$file_name3 = $_FILES['sidebar3']['name'];
+$tempname3 = $_FILES['sidebar3']['tmp_name'];
+
+
+
+$folder = 'postImages/' . $file_name;
+$folder1 = 'postImages/' . $file_name1;
+$folder2 = 'postImages/' . $file_name2;
+$folder3 = 'postImages/' . $file_name3;
+
+move_uploaded_file($tempname, $folder);
+move_uploaded_file($tempname1, $folder1);
+move_uploaded_file($tempname2, $folder2);
+move_uploaded_file($tempname3, $folder3);
